060ed564a89b4fb32573957d794f16b0c9f538bd
Chirag Singh <chiraaajsingh7600@gmail.com>
2026-06-14 17:27:50 +0530

    some changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "doorstep-repair",
       script: "dist/index.cjs",
+      node_args: "--env-file=.env",
       env: {
         NODE_ENV: "production",
       }
